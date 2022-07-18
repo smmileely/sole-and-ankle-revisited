7a53a5fae2badce463c29b1d1b830e39c17c79ad
Luyao <smmileely@gmail.com>
2022-07-18 19:00:11 +0800

    Done Exercise 1 Breakpoints

--- src/constants.js
+++ src/constants.js
@@ -16,3 +16,15 @@ export const WEIGHTS = {
   medium: 600,
   bold: 800,
 };
+
+export const BREAKPOINTS = {
+  laptopMax: 1300,
+  tabletMax: 950,
+  phoneMax: 600,
+};
+
+export const QUERIES = {
+  laptopAndSmaller: `(max-width: ${BREAKPOINTS.laptopMax / 16}rem)`,
+  tabletAndSmaller: `(max-width: ${BREAKPOINTS.tabletMax / 16}rem)`,
+  phoneAndSmaller: `(max-width: ${BREAKPOINTS.phoneMax / 16}rem)`,
+};
