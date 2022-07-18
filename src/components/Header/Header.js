97263c5f8bf31f3bf9d743ed892741b194696f20
Luyao <smmileely@gmail.com>
2022-07-18 22:59:08 +0800

    Done exercise 2 Mobile Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { COLORS, WEIGHTS } from '../../constants';
+import { COLORS, QUERIES, WEIGHTS } from '../../constants';
 import Logo from '../Logo';
+import Icon from '../Icon';
+import UnstyledButton from '../UnstyledButton';
 import SuperHeader from '../SuperHeader';
 import MobileMenu from '../MobileMenu';
+import VisuallyHidden from '../VisuallyHidden';
+import { ShoppingBag } from 'react-feather';
 
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = React.useState(false);
@@ -18,18 +22,34 @@ const Header = () => {
     <header>
       <SuperHeader />
       <MainHeader>
-        <Side>
+        <LogoWrapper>
           <Logo />
-        </Side>
-        <Nav>
-          <NavLink href="/sale">Sale</NavLink>
-          <NavLink href="/new">New&nbsp;Releases</NavLink>
-          <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
-          <NavLink href="/collections">Collections</NavLink>
-        </Nav>
-        <Side />
+        </LogoWrapper>
+        <DesktopNav>
+          <NavLink href='/sale'>Sale</NavLink>
+          <NavLink href='/new'>New&nbsp;Releases</NavLink>
+          <NavLink href='/men'>Men</NavLink>
+          <NavLink href='/women'>Women</NavLink>
+          <NavLink href='/kids'>Kids</NavLink>
+          <NavLink href='/collections'>Collections</NavLink>
+        </DesktopNav>
+        <MobileActions>
+          <UnstyledButton>
+            <ShoppingBagButton>
+              <Icon id='shopping-bag' />
+              <VisuallyHidden>Open Cart</VisuallyHidden>
+            </ShoppingBagButton>
+          </UnstyledButton>
+          <UnstyledButton>
+            <Icon id='search' />
+            <VisuallyHidden>Search</VisuallyHidden>
+          </UnstyledButton>
+          <UnstyledButton>
+            <Icon id='menu' />
+            <VisuallyHidden>Menu</VisuallyHidden>
+          </UnstyledButton>
+        </MobileActions>
+        <Filler />
       </MainHeader>
 
       <MobileMenu
@@ -46,16 +66,68 @@ const MainHeader = styled.div`
   padding: 18px 32px;
   height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  border-top: 4px solid ${COLORS.gray[900]};
+
+  @media ${QUERIES.tabletAndSmaller} {
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  @media ${QUERIES.phoneAndSmaller} {
+    padding-left: 16px;
+    padding-right: 16px;
+  }
 `;
 
-const Nav = styled.nav`
+const DesktopNav = styled.nav`
   display: flex;
   gap: 48px;
   margin: 0px 48px;
+
+  @media ${QUERIES.tabletAndSmaller} {
+    display: none;
+  }
 `;
 
-const Side = styled.div`
+const MobileActions = styled.nav`
+  /* desktop first */
+  display: none;
+
+  @media ${QUERIES.tabletAndSmaller} {
+    display: flex;
+    gap: 32px;
+  }
+
+  @media ${QUERIES.phoneAndSmaller} {
+    gap: 16px;
+  }
+`;
+
+// for the search button to be in the middle visually
+const ShoppingBagButton = styled(UnstyledButton)`
+  /* shifting sth without affecting other children */
+  transform: translateX(-2px);
+`;
+
+// in order to squeeze all the navlinks to the center, both logo and the empty space will grow
+// const Side = styled.div`
+//   flex: 1;
+// `;
+
+const LogoWrapper = styled.div`
   flex: 1;
+
+  @media ${QUERIES.tabletAndSmaller} {
+    flex: revert;
+  }
+`;
+
+const Filler = styled.div`
+  flex: 1;
+
+  @media ${QUERIES.tabletAndSmaller} {
+    display: none;
+  }
 `;
 
 const NavLink = styled.a`
