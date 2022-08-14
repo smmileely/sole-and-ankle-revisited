00b730bb1eeea7e3bfff5bedeff63d7567ad32a0
Luyao <smmileely@gmail.com>
2022-08-14 23:40:09 +0800

    Done exercise 4

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -43,7 +43,7 @@ const Header = () => {
             <Icon id='search' />
             <VisuallyHidden>Search</VisuallyHidden>
           </UnstyledButton>
-          <UnstyledButton>
+          <UnstyledButton onClick={() => setShowMobileMenu(true)}>
             <Icon id='menu' />
             <VisuallyHidden>Menu</VisuallyHidden>
           </UnstyledButton>
